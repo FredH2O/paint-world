0abdda3fae93142f9547910147c34708d9a6dbce
Fred <huertaserick@yahoo.ie>
2025-10-30 19:07:01 +0000

    Initial commit for making my custom theme

---
+++ wp-content/themes/paint-world/footer.php
@@ -0,0 +1,7 @@
+<footer>
+    <p>&copy; <?php echo date('Y') ?> Paint-World. All rights reserved.</p>
+</footer>
+<?php wp_footer(); ?>
+</body>
+
+</html>
---
+++ wp-content/themes/paint-world/index.php
@@ -0,0 +1,12 @@
+<?php
+get_header();
+?>
+
+<main>
+    <h1>Hello from Paint-World!</h1>
+    <p>Welcome to your custom theme.</p>
+</main>
+
+<?php
+get_footer();
+?>
