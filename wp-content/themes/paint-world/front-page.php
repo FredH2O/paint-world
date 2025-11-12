8194fd407085e776577f71f92c5f02545edb619f
Fred <huertaserick@yahoo.ie>
2025-11-12 18:49:30 +0000

    Theme Astra installed for experimenting

--- wp-content/themes/paint-world/front-page.php
+++ wp-content/themes/paint-world/front-page.php
@@ -2,9 +2,15 @@
 
 <main class="home-page">
     <section class="hero">
+
         <?php
         $home_page = get_page_by_path('home'); // your page slug
-        if ($home_page) : ?>
+        if ($home_page) :
+
+            // get feature image for hero
+            $thumb_id = get_post_thumbnail_id($home_page->ID);
+            $thumb_url = wp_get_attachment_url();
+        ?>
 
             <h1 class="page-title"><?php echo esc_html($home_page->post_title); ?></h1>
             <div>
